cdf653a3c8407e791336a4a40ad30502838a8459
Naz Kin <nazkin@pop-os.localdomain>
2020-08-11 16:04:54 -0400

    implemented auth on the front-end

---
+++ server.js
@@ -0,0 +1,46 @@
+
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+const cors = require('cors');
+const admin = require('firebase-admin');
+const PORT = 8000;
+const serviceAccount = require('./adminConfig/fbServiceAccountKey.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://sdkactivity-6ae10.firebaseio.com"
+  });
+
+
+const checkAuth = (req, res, next) => {
+    if (req.headers.authtoken) {
+        admin.auth().verifyIdToken(req.headers.authtoken)
+          .then(() => {
+            next();
+          }).catch(() => {
+            res.status(403).send('Unauthorized');
+          });
+      } else {
+        res.status(403).send('Unauthorized');
+      }
+}
+
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
+
+app.get('/', (req, res)=> {
+    res.json({
+        message: 'Hello there'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log('Port listening on port '+ PORT);
+})
+
+
+
